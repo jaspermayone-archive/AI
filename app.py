2a8deebc6ae6454ee5b7cdc28abb8477d6a6528c
Saptak Bhoumik <saptakbhoumik@gmail.com>
2021-05-15 17:06:32 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+from PyQt5 import QtWidgets,QtGui,QtCore
+import os
+import sys
+
+class GUI(QtWidgets.QMainWindow): 
+
+    def __init__(self):
+        super(GUI, self).__init__()
+        self.setFixedSize(205, 205)
+        self.setWindowTitle("JARVIS")
+        self.setWindowIcon(QtGui.QIcon(os.path.join('images', 'logo.png')))
+        self.setStyleSheet("background : white;text-align: center;")
+
+
+        self.nav = QtWidgets.QToolBar("Navigation")
+        self.nav.setOrientation(QtCore.Qt.Vertical)
+        self.nav.setIconSize(QtCore.QSize(200, 200))
+        self.nav.setMovable(False)
+        self.addToolBar(QtCore.Qt.RightToolBarArea, self.nav)
+
+        self.mic = QtWidgets.QAction(QtGui.QIcon(os.path.join('images', 'mic.jpg')), "Add new tab", self)
+
+        self.nav.addAction(self.mic)
+        
+        self.nav.setStyleSheet("""
+                                QToolBar {
+                                    background-color: #ffffff; 
+                                    color:#000000;
+                                }
+                                QToolBar QToolButton {
+                                    background-color: #ffffff;
+                                    border-radius: 200px;
+                                }
+                                
+                                """)
+
+
+
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    app.setApplicationName("JARVIS")
+    gui = GUI()
+    gui.show()
+    sys.exit(app.exec_())
