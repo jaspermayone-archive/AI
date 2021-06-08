284694550863cb58f5042a4cafeac8f4a898ca89
Saptak Bhoumik <saptakbhoumik@gmail.com>
2021-06-08 21:50:30 +0530

    Delete chat.py

--- chat.py
+++
@@ -1,9 +0,0 @@
-from transformers import BlenderbotTokenizer, BlenderbotForConditionalGeneration
-mname = 'facebook/blenderbot-400M-distill'
-model = BlenderbotForConditionalGeneration.from_pretrained(mname)
-tokenizer = BlenderbotTokenizer.from_pretrained(mname)
-def chat(query):
-    inputs = tokenizer([query], return_tensors='pt')
-    reply_ids = model.generate(**inputs)
-    return tokenizer.batch_decode(reply_ids,skip_special_tokens=True)[0]
-
