e16facb9894dc342056ef7a33aa85085e3d080ed
Saptak Bhoumik <saptakbhoumik@gmail.com>
2021-06-08 21:49:12 +0530

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-
-__project__      = 'J.A.R.V.I.S'
-__version__      = '0.0.1'
-__keywords__     = ['jarvis', 'J.A.R.V.I.S', 'ai', 'virtual assistant']
-__author__       = 'Stark industries'
-__author_email__ = 'saptakbhoumik@gmail.com'
-__platforms__    = 'WINDOWS'
