87512ad099ed1c687655a88d4b4e250241061034
Saptak Bhoumik <saptakbhoumik@gmail.com>
2021-06-18 18:46:50 +0530

    Add files via upload

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,8 +1,8 @@
 from __future__ import absolute_import
 
 __project__      = 'J.A.R.V.I.S'
-__version__      = '0.0.1'
+__version__      = '0.0.2'
 __keywords__     = ['jarvis', 'J.A.R.V.I.S', 'ai', 'virtual assistant']
 __author__       = 'Adapt Development'
 __author_email__ = 'saptakbhoumik@gmail.com'
-__platforms__    = 'WINDOWS'
+__platforms__    = 'WINDOWS|LINUX'
